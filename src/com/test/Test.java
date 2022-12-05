78b02ecdc9db6d427bc92d482421aaa4d39ea166
Akshay Phatale <akshayphatale3@gmail.com>
2022-12-05 19:42:45 +0530

    wh-101 update

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		Employee emp=new Employee(101, "mahesh");
 		System.out.println(emp);
-		System.out.println("Modification");
+	
 	}
 
 }
